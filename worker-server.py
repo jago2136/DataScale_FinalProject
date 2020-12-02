dc6fbe73eba6608555d009113c21c7ac37da59e7
fultonowen <34138924+fultonowen@users.noreply.github.com>
2020-12-02 13:40:46 -0700

    working w rabbitmq and Redis TEST

--- worker-server.py
+++ worker-server.py
@@ -10,10 +10,28 @@ import json
 ######################################################################################
 
 # Packages needed for to execute
-# usps-api
+# pip install usps-api
+redisHost = os.getenv("REDIS_HOST") or "localhost"
+rabbitMQHost = os.getenv("RABBITMQ_HOST") or "localhost"
+#checking connection and establishing channel for communication
+print("Connecting to rabbitmq({}) and redis({})".format(rabbitMQHost,redisHost))
+connection=pika.BlockingConnection(pika.ConnectionParameters(host=rabbitMQHost))
+channel=connection.channel()
 
-# USPS Login
-usps = USPSApi('777PLATI6457')
+channel.exchange_declare(exchange='worker_exchange', exchange_type='direct')
+result=channel.queue_declare(queue='', durable=True)
+queue_n=result.method.queue
+channel.queue_bind(exchange='worker_exchange', queue=queue_n, routing_key='worker')
+
+
+
+#connect to redis database
+trackingNumtoInfo = redis.Redis(host=redisHost, db=1)
+
+
+# USPS Login and Order class structure
+uspsLogin='777PLATI6457'
+usps = USPSApi(uspsLogin)
 
 class Order:
 	trackingTime = None
@@ -30,41 +48,47 @@ def getTrackingInfo(order, number):
 		order.trackingTime = trackingInfo.result['TrackResponse']['TrackInfo']['TrackDetail'][0]['EventTime']
 		order.trackingDate = trackingInfo.result['TrackResponse']['TrackInfo']['TrackDetail'][0]['EventDate']
 		order.trackingEvent = trackingInfo.result['TrackResponse']['TrackInfo']['TrackDetail'][0]['Event']
+		#print("\nOrder Info\nTime: ", order.trackingTime, "\nDate: ", order.trackingDate, "\nEvent: ", order.trackingEvent, "\n")
+		return
 
 	except:
 		order.trackingTime = 'Error'
 		order.trackingDate = 'Error'
 		order.trackingEvent = 'Error'
-		return except
+		return
 
-newOrder = Order()
-getTrackingInfo(newOrder, '940010820549753006850')
-print("\nOrder Info\nTime: ", newOrder.trackingTime, "\nDate: ", newOrder.trackingDate, "\nEvent: ", newOrder.trackingEvent, "\n")
-
-## TEMP FROM LAB 7
-# @app.route('/match/<hashFromClient>', methods = ['GET'])
 def serv_match(trackingNumber):
 
 	newOrder = Order()
 	# Parse received request
-    try:
-
-    	getTrackingInfo(newOrder, trackingNumber)
-    	response = { 'trackingTime' : order.trackingTime,
-    				 'trackingDate' : order.trackingDate,
-    				 'trackingEvent' : order.trackingEvent
-    				}
-    except:
-    	response = { 'trackingTime' : 'Error',
-    				 'trackingDate' : 'Error',
-    				 'trackingEvent' : 'Error'
-    				}
-
-    # eEncode response
-    response_pickled = jsonpickle.encode(response)
-
-    # Send response
-    return Response(response = response_pickled, status = 200, mimetype = "application/json")
+	try:
+		getTrackingInfo(newOrder, trackingNumber)
+		response = { 'trackingTime' : newOrder.trackingTime,
+					 'trackingDate' : newOrder.trackingDate,
+					 'trackingEvent' : newOrder.trackingEvent
+					}
+	except:
+		response = { 'trackingTime' : 'Error',
+					 'trackingDate' : 'Error',
+					 'trackingEvent' : 'Error'
+					}
+	return response
 
 
+#when worker receive data from the server
+def callback(ch, method, properties, body):
+	serverMessage=jsonpickle.decode(body)
+	print("Received message: ", serverMessage)
+	tracking_str=serverMessage['tracking_num']
+	if (tracking_str!="none"):
+		response=serv_match(tracking_str)
+		trackingNumtoInfo.set(trackingNumber,response)
+
+	print("[x] Done")
+	ch.basic_ack(delivery_tag=method.delivery_tag)
+
+#pub-sub consumption by workers
+channel.basic_consume(queue=queue_n, on_message_callback=callback, auto_ack=False)
+
+channel.start_consuming()
 
