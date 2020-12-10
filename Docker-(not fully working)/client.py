803aa7244a18413ccb4b81ba50f1397a3496cd34
fultonowen <34138924+fultonowen@users.noreply.github.com>
2020-12-09 18:21:32 -0700

    Docker upload

---
+++ Docker-(not fully working)/client.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import requests
+import json
+import sys
+import jsonpickle
+
+
+def putTrackInfo(host_addr, tracknum):
+	headers= {'content-type':'application/json'}
+	url=host_addr+'/track'
+	number=jsonpickle.encode({"number":tracknum})
+	response=requests.post(url, data=number, headers=headers)
+	print("Response is", response)
+	print(json.loads(response.text))
+
+def getHomepage(host_addr):
+	headers= {'content-type':'application/json'}
+	url=host_addr+'/getInfoHomepage'
+	response=requests.get(url, headers=headers)
+	print("Response is", response)
+	print(json.loads(response.text))
+
+def update(host_addr):
+	headers= {'content-type':'application/json'}
+	url=host_addr+'/update'
+	response=requests.get(url, headers=headers)
+	print("Response is", response)
+	print(json.loads(response.text))
+
+host=sys.argv[1]
+addr="http://"+host+":5000"
+command=sys.argv[2]
+tracking_num=sys.argv[3]
+
+if command=='track':
+	putTrackInfo(addr,tracking_num)
+elif command=='getInfo':
+	getHomepage(addr)
+elif command=='update':
+	update(addr)
+else:
+	print("Command not recognized")
