bcbaca4d85e472e0f37795d365e867dad51e2b01
Owen Fulton <owfu0013@colorado.edu>
2020-12-09 18:50:45 -0700

    travis testing

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
+#!/usr/bin/env python3
+import requests
+import json
+import sys
+import jsonpickle
 import unittest
 
 class test_build(unittest.TestCase):
 
 	#test connection to server
 	def test_1(self):
-	
-	#test a working tracking number	
-	def test_2(self):
-		
-
-	def test_3(self):
-		
-
+		headers= {'content-type':'application/json'}
+		url='http://localhost:5000'+'/track/'+str(123)
+		response=requests.get(url, headers=headers)
+		res=json.loads(response)
+		self.assertEqual(res['server'], 'success')
+		#test a working tracking number	
 
 if __name__ == '__main__':
 	unittest.main()
